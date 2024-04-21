a06f2a6ab4c354f95eede9d17b36cec3d82888a5
HajrahSiddiqui <167708894+HajrahSiddiqui@users.noreply.github.com>
2024-04-21 15:09:10 -0400

    MultiplicationQuizzTool

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiplicationQuizzTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
